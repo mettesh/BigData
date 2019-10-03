16f2297e0721f5038660cc02acf8e78b84679824
Mette <mette-strand@live.no>
2019-10-03 17:53:30 +0200

    finished task 2

--- Java/src/main/java/Two_AddrStreetTagsPerStreet.java
+++ Java/src/main/java/Two_AddrStreetTagsPerStreet.java
@@ -1,90 +1,240 @@
 import java.io.IOException;
+import java.io.StringReader;
+
+import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.DataOutputBuffer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.*;
 
-public class Two_AddrStreetTagsPerStreet { //lecture 10
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+import javax.xml.parsers.*;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+import org.xml.sax.InputSource;
+
+
+
+public class Two_AddrStreetTagsPerStreet {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.addResource("hdfs-site.xml");
+
+        conf.set("startTag", "<osm");
+        conf.set("endTag", "</osm>");
+
+
+        Job job = Job.getInstance(conf, "xml count");
+
+        job.setJarByClass(Two_AddrStreetTagsPerStreet.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setInputFormatClass(StartEndFileInputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class StartEndFileInputFormat extends FileInputFormat < LongWritable, Text > {
+
+        @Override
+        public RecordReader < LongWritable, Text > createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
+
+            StartEndRecordReader reader = new StartEndRecordReader();
+            reader.initialize(split, context);
+
+            return reader;
+        }
+
+    }
+
+    public static class StartEndRecordReader extends RecordReader < LongWritable, Text > {
+
+        private long start;
+        private long pos;
+        private long end;
+        private FSDataInputStream fsin;
+        private byte[] startTag;
+        private byte[] endTag;
+        private LongWritable key = new LongWritable();
+        private Text value = new Text();
+        private final DataOutputBuffer buffer = new DataOutputBuffer();
+
+
+        @Override
+        public void initialize(InputSplit genericSplit, TaskAttemptContext context) throws IOException {
+            FileSplit split = (FileSplit) genericSplit;
+
+
+            Configuration job = context.getConfiguration();
+            this.startTag = job.get("startTag").getBytes("utf-8");
+            this.endTag = job.get("endTag").getBytes("utf-8");
+
+
+            start = split.getStart();
+            end = start + split.getLength();
+
+            final Path file = split.getPath();
+            FileSystem fs = file.getFileSystem(job);
+            this.fsin = fs.open(split.getPath());
+            fsin.seek(start);
+        }
+
+
+        @Override
+        public boolean nextKeyValue() throws IOException {
+            if (fsin.getPos() < end) {
+                if (readUntilMatch(startTag, false)) {
+                    try {
+                        buffer.write(startTag);
+                        if (readUntilMatch(endTag, true)) {
+                            key.set(fsin.getPos());
+                            value.set(buffer.getData(), 0, buffer.getLength());
+                            return true;
+                        }
+                    } finally {
+                        buffer.reset();
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean readUntilMatch(byte[] match, boolean withinBlock) throws IOException {
+            int i = 0;
+            while (true) {
+                int b = fsin.read();
+                // end of file:
+                if (b == -1) return false;
+                // save to buffer:
+                if (withinBlock) buffer.write(b);
+
+                // check if we're matching:
+                if (b == match[i]) {
+                    i++;
+                    if (i >= match.length) return true;
+                } else i = 0;
+                // see if we've passed the stop point:
+                if (!withinBlock && i == 0 && fsin.getPos() >= end) return false;
+            }
+        }
+
+
+
+        @Override
+        public LongWritable getCurrentKey() throws IOException,
+                InterruptedException {
+            return key;
+        }
+
+        @Override
+        public Text getCurrentValue() throws IOException,
+                InterruptedException {
+            return value;
+        }
+
+
+        @Override
+        public float getProgress() throws IOException,
+                InterruptedException {
+            if (start == end) {
+                return 0.0f;
+            } else {
+                return Math.min(1.0f, (pos - start) / (float)(end - start));
+            }
+        }
+
+
+        @Override
+        public void close() throws IOException {
+            if (fsin != null) {
+                fsin.close();
+            }
+        }
+    }
+
+    public static class TokenizerMapper
+            extends Mapper < Object, Text, Text, IntWritable > {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        //Metode vi overrider fra Mapper
-        @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(Object key, Text value, Context context) throws IOException,
+                InterruptedException {
 
-            // Må gjøre om text-verdien om til String (Da vi ikke kan jobbe med text-datatypen)
-            // Vi må deretter dele denne linjen med text opp i ord ved å benytte StringTokenizer
-            // Kan her sette hva vi skal dele på:
-            StringTokenizer itr = new StringTokenizer(value.toString(), " /-?!:,()[]{}.\"");
+            try {
 
-            // Går nå igjennom alle ordene:
-            while (itr.hasMoreTokens()) {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
 
-                // Legger et ord i aWord og gjør det til lowerCase
-                String aWord = itr.nextToken().toLowerCase();
+                InputSource is = new InputSource(new StringReader(value.toString()));
 
-                //Om et av ordene er tomme ønsker jeg å gå videre (Vil ikke ha denne som nøkkel)
-                if(aWord == ""){
-                    continue;
+                Document document = builder.parse(is);
+
+                document.getDocumentElement().normalize();
+
+                Element root = document.getDocumentElement();
+
+                // Henter ut key i alle tag-er
+                NodeList tag = root.getElementsByTagName("tag");
+
+                // Itererer igjennom alle tag-ene
+                for(int i = 0; i < tag.getLength(); i++){
+
+                    // Henter ut k-attributten for nåværende Tag
+                    String tagAttributeKey = tag.item(i).getAttributes().getNamedItem("k").getTextContent();
+
+                    if(tagAttributeKey != null && tagAttributeKey.equals("addr:street")){
+                        String tagAttributeValue = tag.item(i).getAttributes().getNamedItem("v").getTextContent();
+
+                        context.write(new Text(tagAttributeValue), one);
+                    }
                 }
-                //hallo
-                // Setter dette ordet til å være text (Da hadoop ikke leser String, men text)
-                word.set(aWord);
 
-                //OPPRETTER KEY,VALUE:
-                // Setter nå en ordet til å være key, og deretter en value
-                // Disse må være på datatype som hadoop forstår (one = intWriteble 1)
-                context.write(word, one);
+
+
+            } catch (SAXException exception) {
+                // ignore
+            } catch (ParserConfigurationException exception) {
+
             }
         }
+
     }
 
-    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    public static class IntSumReducer
+            extends Reducer < Text, IntWritable, Text, IntWritable > {
         private IntWritable result = new IntWritable();
 
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
+        public void reduce(Text key, Iterable < IntWritable > values, Context context) throws IOException, InterruptedException {
 
-            // Går igjennom alle verdiene i values til denne nøkkelen
-            for (IntWritable val : values) {
-                // Legger til denne verdien i sum
+            int sum = 0;
+            for (IntWritable val: values) {
                 sum += val.get();
             }
             result.set(sum);
-
-            // Setter nå sammen nøkkelen med alle verdiene til denne (summen som ble lagt sammen over)
             context.write(key, result);
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        if (args.length != 2){
-            System.err.println("Invalid arguments!\n");
-        }
-
-        Configuration conf = new Configuration();
-        conf.addResource("hdfs-site.xml");
-
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(Two_AddrStreetTagsPerStreet.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
