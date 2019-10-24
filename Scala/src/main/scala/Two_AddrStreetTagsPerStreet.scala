eac53536c98e5d7c9525a4b3133c611a050b58d3
Mette <mette-strand@live.no>
2019-10-24 14:32:24 +0200

    Implemented task 2 - Scala

--- Scala/src/main/scala/Two_AddrStreetTagsPerStreet.scala
+++ Scala/src/main/scala/Two_AddrStreetTagsPerStreet.scala
@@ -2,17 +2,32 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Two_AddrStreetTagsPerStreet {
+  
+  // How many addr:street tags exist for each street?
 
   def main(args: Array[String]) {
 
+    //Initaliserer spark
     val spark: SparkSession = initializeSpark(args)
+
+    //Henter inn data basert på ønskede tagger:
     val nodeData : DataFrame = searchForNodeAndTag(spark)
 
+    import spark.implicits._
+
+    // Henter ut attributt k med dens verdi til tag man er i.
+    // Plukker ut de taggene hvor attributten er addr:street
+    val value = nodeData.select("_k", "_v").filter($"_k" === "addr:street")
+
+    // Grupperer så alle gatene etter verdien. Og teller disse
+    val next = value.groupBy("_v").count()
+
+    next.show()
   }
 
   private def searchForNodeAndTag(spark : SparkSession) = {
     val nodeData = spark.read.format("com.databricks.spark.xml")
-      .option("rootTag", "node")
+      .option("rootTag", "osm")
       .option("rowTag", "tag")
       .load("input/oslo.osm")
 
@@ -20,8 +35,7 @@ object Two_AddrStreetTagsPerStreet {
   }
 
   private def initializeSpark(args: Array[String]) = {
-
-    val conf = new SparkConf().setMaster(args(0)).setAppName("Task2")
+    val conf = new SparkConf().setMaster(args(0)).setAppName("Two - How many addr:street tags exist for each street?")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
 
